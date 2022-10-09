33bd19ec40cd1fd557b832fe68682ac3463c54d6
gby211 <gby211@yandex.ru>
2022-10-09 18:05:06 +0300

    move

--- app/src/main/java/com/example/weatherapponcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapponcompose/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weatherapponcompose
+package com.example.weatherapponcompose.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherapponcompose.ui.theme.WeatherAppOnComposeTheme
+import com.example.weatherapponcompose.presentation.ui.theme.WeatherAppOnComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
